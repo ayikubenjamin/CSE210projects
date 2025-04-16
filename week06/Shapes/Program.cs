d78e8f1765a836f56fe3e73092d71bc729e07112
ayikubenjamin <ayikutetteh70@gmail.com>
2025-04-16 18:05:12 +0000

    week 06 lesson

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square square1 = new Square("Red", 4);
+        Console.WriteLine($"{square1.GetColor()}");
+        Console.WriteLine($"{square1.GetArea()}");
+
+        Rectangle rectangle1 = new Rectangle("Yellow", 12, 9);
+        Console.WriteLine($"{rectangle1.GetColor()}");
+        Console.WriteLine($"{rectangle1.GetArea()}");
+
+        Circle circle1 = new Circle("Green", 7);
+        Console.WriteLine($"{circle1.GetColor()}");
+        Console.WriteLine($"{circle1.GetArea()}");
+
+        List<Shape>shapes = new List<Shape>();
+        shapes.Add(rectangle1);
+        shapes.Add(square1);
+        shapes.Add(circle1);
+
+        foreach(Shape shape in shapes)
+        {
+           Console.WriteLine($"color: {shape.GetColor()}");
+           Console.WriteLine($"Area: {shape.GetArea()}");  
+            
+        }
     }
 }
